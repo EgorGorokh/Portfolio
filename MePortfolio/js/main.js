9b466580381a0b2831cb40d8fae6203b84003682
Egor Gorokh <82266177+EgorGorokh@users.noreply.github.com>
2022-01-02 20:30:37 +0300

    Add files via upload

---
+++ MePortfolio/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+$(function(){
+
+    $(".head_inner nav ul li.switch a").on("click", function(e){
+        $("body").toggleClass("switchMode");
+        e.preventDefault();
+    });
+
+
+
+});
